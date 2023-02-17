2a8e274013592128cdd7daffcac86473a1bccf80
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2023-02-18 02:45:43 +0300

    Provide all the dependencies for API interaction

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
     application
 }
 
@@ -10,6 +11,20 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+}
+
+val ktorVersion: String by project
+val kotlinxSerializationVersion: String by project
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
